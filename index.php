2856bba9350667641dcaca7295cf13c62810f3eb
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-04-13 13:09:03 -0300

    adicionando input com ajax

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php 
+  if (isset($_POST['search'])){
+    $con = new mysqli('localhost','root','','jquery-autocomplete');
+
+    
+    $response = "Sem dados encontrados";
+
+    exit( $response);
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <title>Jquery Ajax PHP</title>
+</head>
+<body>
+  <input type="text" placeholder="Procure aqui..." id="searchBox">
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"  crossorigin="anonymous"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#searchBox').keyup(function() {
+        var query = $('#searchBox').val();
+
+        if(query.length > 2){
+          $.ajax(
+            {
+              url: 'index.php',
+              method: 'POST',
+              data: {
+                search: 1,
+                q: query
+              },
+              success: function(data) {
+                console.log(data);
+              },
+              dataType: 'text'
+            }
+          );
+        }
+        
+      })
+    });
+  </script>
+</body>
+</html>
